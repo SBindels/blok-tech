81512c297496cf4c7fd9234eb1d03155c2050763
SBindels <bindelssjoerd@gmail.com>
2022-02-15 13:28:21 +0100

    first routes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const app = express()
+const port = 3000
 
-express()
-    .get('/', onhome)
-    .listen(1900)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-function onhome(req, res) {
-    res.send('<h1>Hello Client</h1>\n')
-}
+app.get('/aboutme', (req, res) => {
+    res.send('this is the about me page!')
+  })
+
+app.get('/login', (req, res) => {
+    res.send('welcome to the login page')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
