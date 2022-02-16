6f3ec6b77114c6189e1ed04e133cec6bee51a3a9
SBindels <bindelssjoerd@gmail.com>
2022-02-16 23:02:29 +0100

    added more handlebars code. begin of templating files

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const app = express()
 const port = 3000
 const handlebars = require('express-handlebars');
 
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
+app.engine('hbs', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs'
     }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
 
 
 app.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {layout : 'index'});
+    res.render('main', {layout : 'main'});
     });
 
 app.get('*', (req, res) => {
