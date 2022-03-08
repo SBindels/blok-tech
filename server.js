6a06d81e10534b3777c4da92e8588c8e0745137c
SBindels <bindelssjoerd@gmail.com>
2022-03-08 13:08:17 +0100

    remove hbs for ejs

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars');
+const esj = require('ejs')
 
-app.engine('hbs', handlebars({
-    layoutsDir: `${__dirname}/views/layouts`,
-    extname: 'hbs'
-    }));
-app.set('view engine', 'hbs');
-app.set('views', './views');
+// app.engine('hbs', handlebars({
+//     layoutsDir: `${__dirname}/views/layouts`,
+//     extname: 'hbs'
+//     }));
+// app.set('view engine', 'hbs');
+// app.set('views', './views');
 
 
-
-app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {layout : 'main'});
-    });
+// app.get('/', (req, res) => {
+//     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+//     res.render('main', {layout : 'main'});
+//     });
 
 app.get('*', (req, res) => {
     res.send('error')
