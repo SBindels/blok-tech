a12bfc20b0086ea6b45a4058aee4728a8e1745e2
SBindels <bindelssjoerd@gmail.com>
2022-03-08 15:11:12 +0100

    functie registratie en login

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars');
-const esj = require('ejs')
+const esj = require('ejs');
+const req = require('express/lib/request');
+
+require('dotenv').config();
 
 //routes
 express()
     .use(express.static('public'))
     .set('view engine', 'ejs')
     .set('views', 'view')
+    .get('/registratie', registratieForm)
+    .get('/login', loginForm)
     .listen(3000);
+
+
+function registratieForm(req, res) {
+    res.render(registratie.ejs)
+}
+
+function loginForm(req, res) {
+    res.render(login.ejs)
+}
 
 app.get('*', (req, res) => {
     res.send('error')
