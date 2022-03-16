fe52993caeb7d13299490e06a1c9dbfd9a8285ab
SBindels <bindelssjoerd@gmail.com>
2022-03-16 17:21:08 +0100

    begin switch to mongoose for passport (compare credentials)

---
+++ config/passport.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const LocalStrategy = require('passport-local').Strategy
+
+//load user model
+const User = require('../models/user');
+
+modules.export = function(passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: 'emailadress'}, (email, wachtwoord, done) => {
+            User.findOne({ email: email })
+        })
+    );
+}
+
