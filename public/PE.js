b1af725f20f9a2f0753135a0845ecb3fdfb6976a
SBindels <bindelssjoerd@gmail.com>
2022-03-18 13:59:33 +0100

    added progressive enhancement to forms

--- public/PE.js
+++ public/PE.js
@@ -1,2 +1,41 @@
 //file for progressive enhancement form
-console.log("hello world");
+
+console.log('hello world');
+
+const form = document.getElementById('form');
+const username = document.getElementById('voornaam');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(usernameValue === ""){
+        setErrorFor(username, 'Voornaam moet worden ingevuld');
+    }else {
+        setSuccessFor(username);
+    }
+    if(emailValue === ""){
+        setErrorFor(email, 'Email moet worden ingevuld');
+    }else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is niet valid')
+    }else{
+        setSuccessFor(email);
+    }
+
+    if(passwordValue === ""){
+        setErrorFor(password, 'Wachtwoord moet worden ingevuld');
+    }else {
+        setSuccessFor(password);
+    }
+
+
+}
