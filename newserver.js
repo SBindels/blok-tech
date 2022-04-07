cd630b90291a3771db068ab419ce0eb18a0bda86
SBindels <bindelssjoerd@gmail.com>
2022-04-07 23:36:03 +0200

    fixed css link

--- newserver.js
+++ newserver.js
@@ -25,7 +25,7 @@ app.use(session({
 }));
 
 app.use(bodyparser.urlencoded({extended: true}));
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
